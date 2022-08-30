7348851eab9f4df0e3676434d4c5b1a05c6db123
MrGentle <pellejensen94@gmail.com>
2022-08-30 21:14:12 +0200

    Add project files.

---
+++ Patches.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+
+namespace SeasonFetcher {
+    public static class Patches {
+        public static class AMKONCFCDOD_Patches {
+            [HarmonyPatch(typeof(AMKONCFCDOD), "HMJDLJKCEPK")]
+            [HarmonyPrefix]
+
+            static void GetLeaderboardID_Patch(string __0) {
+                if (__0 != "cheaters") SeasonFetcher.season.LeaderboardName = __0;
+            }
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,22 @@
+﻿using BepInEx;
+using HarmonyLib;
+using BepInEx.Logging;
+using UnityEngine;
+
+namespace BlazeDiscordRPC
+{
+    [BepInPlugin("no.gentle.plugin.blazediscordrpc", "BlazeDiscordRPC", "0.0.1")]
+    public class SeasonFetcher : BaseUnityPlugin {
+        internal static SeasonFetcher Instance { get; private set; } = null;
+        internal static ManualLogSource Log { get; private set; } = null;
+        public static Harmony harmony = new(PluginInfo.PLUGIN_NAME);
+
+        private void Awake() {
+            Instance = this;
+            Log = Logger;
+            Log.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+
+            //harmony.PatchAll(typeof(Patches.));
+        }
+    }
+}
